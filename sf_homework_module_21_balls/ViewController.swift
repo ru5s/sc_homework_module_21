ce3e76f5d751a854a56417d5bb22f965984f333e
ru5s <hellperock@gmail.com>
2022-07-16 15:03:30 +0500

    Initial Commit

---
+++ sf_homework_module_21_balls/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sf_homework_module_21_balls
+//
+//  Created by Ruslan Ismailov on 16/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
